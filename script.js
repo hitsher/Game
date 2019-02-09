a82eb4c955a4d11a411344a1b5e37ab09947900a
Pawel Dzienny <pawellose994@gmail.com>
2019-02-09 15:18:48 +0100

    Add meteors

--- script.js
+++ script.js
@@ -1,20 +1,15 @@
 const canvasElem = document.getElementById('stage');
 const ctx = canvasElem.getContext('2d');
+let points = 0;
+let addMove = 1;
 
-canvasElem.width = innerWidth*0.8;
-canvasElem.height = canvasElem.width/2;
-
-
-const meteorMove = meteorsGame => {
-    meteorsGame.forEach(meteorGame => {
-        meteorGame.move(collisionObjects)
-    })
-};
+canvasElem.width = innerWidth * 0.8;
+canvasElem.height = canvasElem.width / 2;
 
 class space {
-    constructor(width, height, color, positionX, positionY) {
-        this.width = width;
-        this.height = height;
+    constructor(color, positionX, positionY) {
+        this.width = canvasElem.width*0.06;
+        this.height = this.width/2;
         this.color = color;
         this.positionX = positionX;
         this.positionY = positionY;
@@ -31,22 +26,32 @@ class Meteor {
     }
 };
 
-const drawObject = (collisionObjects, context) => {
-    collisionObjects.forEach(collisionObject => {
-        context.fillStyle = collisionObject.color;
-        context.fillRect(collisionObject.positionX, collisionObject.positionY, collisionObject.width, collisionObject.height)
-    });
+function drawObjects(objectsToDraw, context) {
+    for (let i = 0; i < objectsToDraw.length; i++) {
+        context.fillStyle = objectsToDraw[i].color;
+        context.fillRect(objectsToDraw[i].positionX, objectsToDraw[i].positionY, objectsToDraw[i].width, objectsToDraw[i].height)
+    };
 }
 
-const collisionObjects = [];
+const objectsToDraw = [];
 const meteorsGame = [];
 
-const playerspace = new space(30, 20, "pink", 50, 50);
-const Meteor1 = new Meteor(20, canvasElem.height*0.5, "black", 110, 50);
-const Meteor11 = new Meteor(20, canvasElem.height*0.5, "black", 110, 50-canvasElem.height);
+const playerspace = new space("pink", 0, 50);
+const Meteor1 = new Meteor(20, canvasElem.height * 0.6, "black", canvasElem.width * 0.2 + 30, 50 - canvasElem.height);
+const Meteor2 = new Meteor(20, canvasElem.height * 0.5, "black", canvasElem.width * 0.2 + 30, 50);
 
-collisionObjects.push(playerspace, Meteor1, Meteor11);
-meteorsGame.push(Meteor1,Meteor11);
+const Meteor3 = new Meteor(20, canvasElem.height * 0.7, "black", canvasElem.width * 0.4 + 30, 150 - canvasElem.height);
+const Meteor4 = new Meteor(20, canvasElem.height * 0.5, "black", canvasElem.width * 0.4 + 30, 150);
+
+const Meteor5 = new Meteor(20, canvasElem.height * 0.5, "black", canvasElem.width * 0.6 + 30, 100 - canvasElem.height);
+const Meteor6 = new Meteor(20, canvasElem.height * 0.8, "black", canvasElem.width * 0.6 + 30, 100);
+
+const Meteor7 = new Meteor(20, canvasElem.height * 0.7, "black", canvasElem.width * 0.8 + 30, 200 - canvasElem.height);
+const Meteor8 = new Meteor(20, canvasElem.height * 0.5, "black", canvasElem.width * 0.8 + 30, 200);
+
+
+objectsToDraw.push(playerspace, Meteor1, Meteor2, Meteor3, Meteor4, Meteor5, Meteor6, Meteor7, Meteor8);
+meteorsGame.push(Meteor1, Meteor2, Meteor3, Meteor4, Meteor5, Meteor6, Meteor7, Meteor8);
 
 let up = false;
 let right = false;
@@ -110,14 +115,11 @@ document.addEventListener('keyup', function (e) {
     }
 });
 
-
-let points = 0;
 function checkCollision(meteorsGame) {
     const spaceLeft = that.positionX;
     const spaceRight = that.positionX + that.width;
     const spaceBottom = that.positionY + that.height;
     const spaceTop = that.positionY;
-    
 
     for (let i = 0; i < meteorsGame.length; i++) {
         let objectRight = meteorsGame[i].positionX + meteorsGame[i].width;
@@ -125,39 +127,46 @@ function checkCollision(meteorsGame) {
         let objectTop = meteorsGame[i].positionY;
         let objectBottom = meteorsGame[i].positionY + meteorsGame[i].height;
         if ((spaceRight > objectLeft) && (spaceTop < objectBottom) && (spaceBottom > objectTop) && (spaceLeft < objectRight)) {
-            alert("Collision!!")
-            that.positionX = 50;
+            that.positionX = 0;
             that.positionY = 50;
-        } 
+            left = false;
+            right = false;
+            up = false;
+            down = false;
+            addMove = 1;
+        }
     }
-    if (spaceRight==canvasElem.width){
-        console.log("Winner!!")
-        that.positionX = 50;
+    if (spaceRight == canvasElem.width) {
+        that.positionX = 0;
         that.positionY = 50;
-        points=points+1;
-        console.log(points)
+        points = points + 1;
+        left = false;
+        right = false;
+        up = false;
+        down = false;
+        addMove = addMove + 1;
     }
 }
 
 function moveObj(meteorsGame) {
     for (let i = 0; i < meteorsGame.length; i++) {
-        let addMove = 4;
         meteorsGame[i].positionY = meteorsGame[i].positionY + addMove;
-        if(meteorsGame[i].positionY>canvasElem.height){
-            meteorsGame[i].positionY=-canvasElem.height}
+        if (meteorsGame[i].positionY > canvasElem.height) {
+            meteorsGame[i].positionY = -canvasElem.height
+        }
     }
 }
 
-
 const run = () => {
     ctx.clearRect(0, 0, canvasElem.width, canvasElem.height);
-    drawObject(collisionObjects, ctx);
+    drawObjects(objectsToDraw, ctx);
+    moveObj(meteorsGame);
     checkCollision(meteorsGame);
-    moveObj(meteorsGame)
+
 }
 
-function timer (){
-let timer = setInterval(run, 1000 / 60);
+function timer() {
+    let timer = setInterval(run, 1000 / 60);
 }
 
 timer()
